f5372cca4305fb097419adfa1b3cf1d8de8e7d1e
Ramon San Gabriel <136789333+RamonSanGabriel@users.noreply.github.com>
2024-11-06 13:50:07 +0800

    Initial commit

---
+++ src/data/home.js
@@ -0,0 +1,5 @@
+export const homeDetails = {
+  name: 'Ramon San Gabriel',
+  position: 'Junior Full Stack Developer',
+  country: 'Philippines',
+};
---
+++ src/data/project.js
@@ -0,0 +1,67 @@
+// import project pictures here
+import project1 from '../images/project-1a.png';
+import project2 from '../images/project-2a.png';
+import project3 from '../images/project-3a.png';
+import project4 from '../images/project-4a.png';
+import project5 from '../images/project-5a.png';
+import project6 from '../images/project-6a.png';
+import project7 from '../images/project-7a.png';
+import project8 from '../images/project-8a.png';
+import project9 from '../images/project-9a.png';
+
+export const projects = [
+  {
+    id: 1,
+    src: project1,
+    name: 'Web Studio',
+    href: 'https://ramonsangabriel.github.io/goit-markup-hw-07/',
+  },
+  {
+    id: 2,
+    src: project2,
+    name: 'Image Gallery',
+    href: 'https://ramonsangabriel.github.io/goit-js-hw-07/01-gallery.html',
+  },
+  {
+    id: 3,
+    src: project3,
+    name: 'Image Search - JavaScript',
+    href: 'https://ramonsangabriel.github.io/goit-js-hw-11/',
+  },
+  {
+    id: 4,
+    src: project4,
+    name: 'Trending Movies',
+    href: 'https://ramonsangabriel.github.io/goit-react-hw-05-movies-lazy',
+  },
+  {
+    id: 5,
+    src: project5,
+    name: 'Phonebook',
+    href: 'https://ramonsangabriel.github.io/goit-react-hw-08-phonebook',
+  },
+  {
+    id: 6,
+    src: project6,
+    name: 'Watch Spot',
+    href: 'https://ramonsangabriel.github.io/team-project-1/',
+  },
+  {
+    id: 7,
+    src: project7,
+    name: 'Image Finder - React',
+    href: 'https://ramonsangabriel.github.io/goit-react-hw-03-image-finder/',
+  },
+  {
+    id: 8,
+    src: project8,
+    name: 'Filmoteka',
+    href: 'https://shanepai-14.github.io/filmoteka-js-team-4',
+  },
+  {
+    id: 9,
+    src: project9,
+    name: 'Slim Mom App',
+    href: 'https://cyan0511.github.io/slim-mom/',
+  },
+];
